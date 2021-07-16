22ed141fb323aa8fb27b18b3d90c4db2079f26f5
Kevin Yen-Kuan Lee <yenkuanlee@gmail.com>
2021-07-16 18:32:27 +0800

    tmp

--- client.go
+++ client.go
@@ -211,7 +211,7 @@ func (client *gocloak) getAdminRealmURL(realm string, path ...string) string {
 	log.Println("KEVIN URL")
 	log.Println(path)
 	if len(path) > 2 {
-		if path[2] == "KEVIN" {
+		if strings.Contains(path[2], "KEVIN") {
 			path = path[:2]
 			path[1] += "?max=1"
 		}
